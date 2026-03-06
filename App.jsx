384daf71cc16ca9edec2963c333aee0c80d2b09d
renato-hue <renato@rvmlaw.com.br>
2026-03-05 23:56:17 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+  <rect width="36" height="64" fill="#111"/>
+  <rect x="36" width="28" height="64" fill="#e2e2e2"/>
+  <text x="4" y="42" font-family="Arial Black, sans-serif" font-size="20" font-weight="900" fill="white">RVM</text>
+  <text x="38" y="42" font-family="Arial, sans-serif" font-size="14" font-weight="400" fill="#888">LAW</text>
+</svg>
